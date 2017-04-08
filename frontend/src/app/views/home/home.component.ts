9c003d29ea29bde194db6ad63c86ec8a735fec9d
Julien Teyteau <iuliust@gmail.com>
2017-04-08 05:51:25 +0200

    style(homeCmp) simplify styles

--- frontend/src/app/views/home/home.component.ts
+++ frontend/src/app/views/home/home.component.ts
@@ -23,6 +23,7 @@ export class HomeComponent implements OnInit {
   }
 
   makeBackendRequest() {
+    if (!isDevMode()) return;
     this.http.post('/api/simulate_call', '')
       .toPromise()
       .then((res: Response) => {
